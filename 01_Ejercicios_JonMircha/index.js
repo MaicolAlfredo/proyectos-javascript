59eef129b80940f8646ca5de16688300c4f9a587
MaicolAlfredo <tegnoblog.17alfrdo@gmail.com>
2024-12-26 19:23:51 +0000

    Cuenta Regresiva

--- 01_Ejercicios_JonMircha/index.js
+++ 01_Ejercicios_JonMircha/index.js
@@ -1,12 +1,14 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarm.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Jan 1, 2025", "Feliz año nuevo");
 });
 
 d.addEventListener("keydown", (e) => {
